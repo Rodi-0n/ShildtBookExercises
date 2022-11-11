5ff5417f4fe8e7e93635e8f63408550e3119bba7
Rodion Yavelov <rodion.yavelov@yandex.ru>
2022-11-11 12:00:42 +0200

    Initial commit

---
+++ src/GalToLit.java
@@ -0,0 +1,10 @@
+public class GalToLit {
+    // convertation gallons ito liters
+    public static void main(String args[]){
+        double gallons; // variable gallons
+        double liters; // variable liters
+        gallons = 10; // number of gallons
+        liters = gallons * 3.7854; // convertation
+        System.out.println(gallons + " gallons is " + liters + " liters");
+    }
+}
